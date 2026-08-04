2a6eed6659199c0ed0e1286a7f518b32c8b5c04b
pratikshac150499-git <pratikshac150499@gmail.com>
2026-08-04 19:55:06 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("HELLO INDIA!");
+    res.end("HELLO WORLD!");
 });
 
 module.exports = server;
